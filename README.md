f023433a731c126a8515454ea9708287e2c53930
Derek Willis <derek.willis@propublica.org>
2019-12-10 15:06:18 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# wvu-projects
-Scripts and data from West Virginia University journalism class
+# WVU Projects
+
+Scripts and data from West Virginia University investigative journalism class in Fall 2019.
+
+### Crime Log
+
+Basic scraper for the WVU campus police [crime log](https://police.wvu.edu/clery-act/campus-safety/crime-log) that outputs a CSV file. The log itself contains the previous 90 days' worth of incidents, so this would be useful for storing information before it disappears from the site.
---
+++ crime-log/crimelog.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+r = requests.get("https://police.wvu.edu/clery-act/campus-safety/crime-log")
+soup = BeautifulSoup(r.text, 'html.parser')
+results = []
+# crime log incidents are enclosed in individual <li> elements
+incidents = soup.find_all('li', attrs={'class': 'incident'})
+
+for incident in incidents:
+    id, title = incident.find('h4').text.split(': ')
+    for p in incident.find_all('p'):
+        year = incident.find_all('p')[0].text
+        datetime = incident.find_all('p')[1].text.split(": ")[1]
+        building = incident.find_all('p')[3].text.split(": ")[1]
+        address = incident.find_all('p')[4].text.split(": ")[1]
+        outcome = incident.find_all('p')[5].text.split(": ")[1]
+    results.append([id, title, year, datetime, building, address, outcome])
+
+with open("crimelog.csv", 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['id', 'title', 'year', 'datetime', 'building', 'address', 'outcome'])
+    writer.writerows(results)
