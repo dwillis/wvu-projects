983feda01ba2fee8dd060ab73b774deac61ed21f
Derek Willis <dwillis@gmail.com>
2019-12-18 09:16:59 -0500

    added crime log updater

--- update_and_commit_all.sh
+++ update_and_commit_all.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-scrape_and_commit () {
+scrape_and_commit_reports () {
     cd wv-legislature
     python agency_reports.py
+    git add *.csv
+    git commit -m "updated reports" && \
+        git push -q https://${GITHUB_PERSONAL_TOKEN}@github.com:dwillis/wvu-projects.git master \
+        || true
+}
+
+scrape_and_commit_crime () {
+    cd crime-log
+    current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+    mv crime_log.csv crime_log.csv.$current_time
+    python crime_log.py
     git add *.csv
     git commit -m "updated reports" && \
         git push -q https://${GITHUB_PERSONAL_TOKEN}@github.com:dwillis/wvu-projects.git master \
@@ -12,4 +23,5 @@ scrape_and_commit () {
 git config --global user.email "wvu_projects@example.com"
 git config --global user.name "WVU Projects"
 
-scrape_and_commit
+scrape_and_commit_reports
+scrape_and_commit_crime
