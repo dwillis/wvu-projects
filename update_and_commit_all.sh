d1e79b9c74dded5e5842bcdcd7570d694543162c
Derek Willis <derek.willis@propublica.org>
2019-12-18 10:05:14 -0500

    pushing

--- update_and_commit_all.sh
+++ update_and_commit_all.sh
@@ -5,19 +5,19 @@ scrape_and_commit_reports () {
     python agency_reports.py
     git add *.csv
     git commit -m "updated reports" && \
-        git push -q https://${GITHUB_PERSONAL_TOKEN}@github.com:dwillis/wvu-projects.git master \
+        git push -q https://github.com:dwillis/wvu-projects.git master \
         || true
 }
 
-scrape_and_commit_crime () {
+scrape_and_commit_crimelog () {
     cd ..
     cd crime-log
     current_time=$(date "+%Y.%m.%d-%H.%M.%S")
     mv crime_log.csv crime_log.csv.$current_time
     python crime_log.py
     git add *.csv
-    git commit -m "updated reports" && \
-        git push -q https://${GITHUB_PERSONAL_TOKEN}@github.com:dwillis/wvu-projects.git master \
+    git commit -m "updated crimelog" && \
+        git push -q https://github.com:dwillis/wvu-projects.git master \
         || true
 }
 
@@ -25,4 +25,4 @@ git config --global user.email "wvu_projects@example.com"
 git config --global user.name "WVU Projects"
 
 scrape_and_commit_reports
-scrape_and_commit_crime
+scrape_and_commit_crimelog
