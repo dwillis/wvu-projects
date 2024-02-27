83a9d74d05f64569c322170b949d328845ff9c8f
Derek Willis <dwillis@gmail.com>
2024-02-27 00:40:14 +0000

    fixed scraper

--- lobbying/lobbying_filings.py
+++ lobbying/lobbying_filings.py
@@ -11,21 +11,18 @@ html = "".join(line.strip() for line in r.text.split('\n'))
 soup = BeautifulSoup(html, 'html.parser')
 results = []
 
-# filings are inside multiple <p> tags
-
-containers = soup.find_all('p')
-
-for container in containers:
-    links = container.find_all('a')
-    for link in links:
+for link in soup.find_all('a'):
+    if hasattr(link, 'href'):
         try:
             url = "https://ethics.wv.gov" + link['href']
             parts = link['href'].split('/')
             period = parts[4]
-            name = parts[5].split('.')[0].replace(period,'').replace('%20',' ').strip()
+            name = parts[6].split('.')[0].replace(period,'').replace('%20',' ').strip().upper()
             results.append([name, period, url])
         except:
             continue
+
+results = [x for x in results if '/SiteCollectionDocuments/Lobbyists' in x[2]]
 
 new_filings = [x for x in results if x[2] not in previous_ids]
 
