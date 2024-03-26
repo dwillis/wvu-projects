ddefcd1acaaaa91d2906d31ead6e968f7501ec45
Derek Willis <dwillis@gmail.com>
2024-03-26 01:42:30 +0000

    added 2024 reports

--- wv-legislature/agency_reports.py
+++ wv-legislature/agency_reports.py
@@ -6,17 +6,20 @@ with open('all_reports.csv', 'r') as existing_reports:
     reader = csv.DictReader(existing_reports)
     previous_urls = [x['url'] for x in reader if x['url'] != 'No Report']
 
-r = requests.get("http://www.wvlegislature.gov/Reports/Agency_Reports/agencylist_all.cfm")
-soup = BeautifulSoup(r.text, 'html.parser')
-results = []
-reports = soup.find_all('tr')[1:-1]
+years = range(2001, 2025)
 
-for report in reports:
-    agency, title, year, filler = [x.text for x in report.find_all('td')]
-    if report.find('a'):
-        results.append([agency, title, year, "http://www.wvlegislature.gov"+report.find('a')['href']])
-    else:
-        results.append([agency, title, year, "No Report"])
+results = []
+
+for year in years:
+    print(year)
+    r = requests.post("http://www.wvlegislature.gov/Reports/Agency_Reports/agencylist_all.cfm", data={'report_year': year})
+    soup = BeautifulSoup(r.text, 'html.parser')
+    reports = soup.find_all('tr')[1:-1]
+
+    for report in reports:
+        agency, title, year, filler = [x.text for x in report.find_all('td')]
+        if report.find('a'):
+            results.append([agency, title, year, "http://www.wvlegislature.gov"+report.find('a')['href']])
 
 all_report_urls = [x[3] for x in results if x[3] != 'No Report']
 new_report_urls = list(set(all_report_urls) - set(previous_urls))
